6a63e2fdde899a8856e7aa221ff5933e6ad390b4
George Thomas <georgefsthomas@gmail.com>
2022-09-20 10:34:01 +0100

    minor refactors

--- evdev/Main.hs
+++ evdev/Main.hs
@@ -20,8 +20,8 @@ main :: IO ()
 main =
     (map T.pack <$> getArgs) >>= \case
         [devPath, ClientId -> clientId, ClientSecret -> clientSecret, RefreshToken -> refreshToken] -> do
-            dev <- newDevice (encodeUtf8 devPath)
-            (pPrint =<<) $ runSpotify (Auth{..}) $ flip S.mapM_ (hoist liftIO $ readEvents dev) \case
+            dev <- newDevice $ encodeUtf8 devPath
+            (pPrint =<<) $ runSpotify Auth{..} $ flip S.mapM_ (hoist liftIO $ readEvents dev) \case
                 Event{eventData} | KeyEvent k Pressed <- eventData -> case k of
                     KeyPlaypause -> do
                         state <- getPlaybackState Nothing
