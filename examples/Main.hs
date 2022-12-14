3615e4bbeb8d5776515fe34dd5506042d9a2b05d
George Thomas <georgefsthomas@gmail.com>
2022-12-14 00:54:49 +0000

    Minor refactor

--- examples/Main.hs
+++ examples/Main.hs
@@ -19,8 +19,10 @@ main = do
             "DeleteRecentPlaylists" : args ->
                 case args of
                     [readMaybe -> Just time] -> pure $ Spotify.DeleteRecentPlaylists.main time
-                    _ -> putStrLn "bad args" >> exitFailure
+                    _ -> badArgs
             x : _ -> putStrLn ("unknown example: " <> x) >> exitFailure
             [] -> putStrLn "no args" >> exitFailure
     auth <- getAuth -- we use the `MonadSpotify IO` instance, as an easy way to get credentials
     either pPrintForceColor pure =<< runSpotify auth example
+  where
+    badArgs = putStrLn "bad args" >> exitFailure
