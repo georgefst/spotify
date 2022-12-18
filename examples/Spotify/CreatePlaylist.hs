georgefst/spotify

commit fb9902c7206af9cd36102b4e8f9984ea19adda26

Add `CreatePlaylist` example

examples/Spotify/CreatePlaylist.hs
@@ -1,0 +1,50 @@
+{- HLINT ignore "Redundant <$>" -}
+module Spotify.CreatePlaylist where
+
+import Spotify
+import Spotify.Servant.Playlists
+import Spotify.Types.Misc
+import Spotify.Types.Search
+import Spotify.Types.Simple
+import Spotify.Types.Tracks
+import Spotify.Types.Users
+
+import Control.Monad (void)
+import Control.Monad.State (MonadIO (liftIO), MonadState (put), MonadTrans (lift), execStateT)
+import Data.List (find)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import Data.Traversable (for)
+import System.Exit (exitFailure)
+
+main :: MonadSpotify m => CreatePlaylistOpts -> m ()
+main opts = do
+    liftIO $ T.putStrLn "Enter lines of artist;track"
+    parsedLines <-
+        (map (T.splitOn ";") . T.lines <$> liftIO T.getContents) >>= traverse \case
+            [a, b] -> pure (a, b)
+            _ -> exit "parse failure"
+    tracks <- for parsedLines \(artist, track) ->
+        maybe (exit "not found") pure
+            =<< (execStateT @_ @(Maybe Track))
+                ( allPages
+                    ( Just \p -> do
+                        if p.offset > searchLimit
+                            then pure False
+                            else case find ((artist `elem`) . map (.name) . (.artists)) p.items of
+                                Just t -> put (Just t) >> pure True
+                                Nothing -> pure False
+                    )
+                    ( \pp ->
+                        maybe (exit "no tracks") pure . (.tracks)
+                            =<< lift (search track [TrackSearch] Nothing pp.limit Nothing pp.offset)
+                    )
+                )
+                Nothing
+    playlist <- flip createPlaylist opts . (.id) =<< getMe
+    void $ addToPlaylist playlist.id Nothing (map (.uri) tracks)
+  where
+    exit s = liftIO $ T.putStrLn s >> exitFailure
+
+searchLimit :: Int
+searchLimit = 250

examples/Main.hs
@@ -3,9 +3,12 @@ module Main (main) where
 import Spotify
 import Spotify.CheckPlaylistOverlap qualified
 import Spotify.CreateArtistLikedSongsPlaylist qualified
+import Spotify.CreatePlaylist qualified
 import Spotify.DeleteRecentPlaylists qualified
+import Spotify.Servant.Playlists (CreatePlaylistOpts (..))
 
 import Data.String (fromString)
+import Data.Text qualified as T
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import Text.Pretty.Simple (pPrintForceColor)
@@ -20,6 +23,17 @@ main = do
                 _ -> badArgs
             "CreateArtistLikedSongsPlaylist" : artists ->
                 pure $ Spotify.CreateArtistLikedSongsPlaylist.main $ map fromString artists
+            "CreatePlaylist" : args -> case args of
+                [T.pack -> name, readMaybe -> Just public, readMaybe -> Just collaborative, T.pack -> description] ->
+                    pure $
+                        Spotify.CreatePlaylist.main
+                            CreatePlaylistOpts
+                                { name
+                                , public
+                                , collaborative
+                                , description
+                                }
+                _ -> badArgs
             "DeleteRecentPlaylists" : args ->
                 case args of
                     [readMaybe -> Just time] -> pure $ Spotify.DeleteRecentPlaylists.main time

spotify.cabal
@@ -123,6 +123,7 @@ executable examples
     other-modules:
         Spotify.CheckPlaylistOverlap
         Spotify.CreateArtistLikedSongsPlaylist
+        Spotify.CreatePlaylist
         Spotify.DeleteRecentPlaylists
     build-depends:
         spotify-types,

commit 0aecf1c358f2fd3454f2f56a6cc0a5e64abaef98

Fix playlist creation search
This has been broken since it was introduced in fb9902c. Though we'd often still get correct results.

examples/Spotify/CreatePlaylist.hs
@@ -39,8 +39,8 @@ main searchType opts = do
                     if p.offset > searchLimit
                         then pure False
                         else case find ((artist `elem`) . map (.name) . getResult) p.items of
-                            Just t -> put (Just t) >> pure True
-                            Nothing -> pure False
+                            Just t -> put (Just t) >> pure False
+                            Nothing -> pure True
                 )
                 ( maybe (exit $ "no " <> itemName <> "s") pure . extractItems
                     <=< lift . search (T.unwords [item, artist]) [searchType] Nothing Nothing